9af29094531ed4fdeb26a22f1890a051e37c76be
Toma Otani <otani.toma@gmail.com>
2020-06-23 13:11:11 -0700

    add sign up function

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -6,8 +6,9 @@ import 'firebase/functions'
 import { firebaseConfig } from './config'
 
 firebase.initializeApp(firebaseConfig);
+
 export const auth = firebase.auth();
-export const db = firestore.firestore();
+export const db = firebase.firestore();
 export const storage = firebase.storage();
 export const functions = firebase.functions();
 export const FirebaseTimestamp = firebase.firestore.Timestamp
--- src/reducks/users/operations.js
+++ src/reducks/users/operations.js
@@ -1,5 +1,6 @@
 import { signInAction } from './actions'
 import { push } from 'connected-react-router'
+import { auth, db, FirebaseTimestamp } from '../../firebase/'
 
 export const signIn = () => {
   return async(dispatch, getState) => {
@@ -22,5 +23,48 @@ export const signIn = () => {
       }))
       dispatch(push('/'))
     }
+  }
+}
+
+export const signUp = (username, email, password, confirmPassword) => {
+  return async (dispatch) => {
+
+    // 入力フォームのバリデーション
+    if (username === '' || email === '' || password === '' || confirmPassword === '') {
+      alert('必須項目が未入力です。')
+      return false
+    }
+
+    if (password !== confirmPassword) {
+      alert('パスワードが一致しません。')
+      return false
+    }
+
+    // ユーザーを作成するメソッドを呼び出す
+    return auth.createUserWithEmailAndPassword(email, password)
+      .then(result => {
+        const user = result.user
+
+        // ユーザーのデータを作成する
+        if (user) {
+          const uid = user.uid
+          const timestamp = FirebaseTimestamp.now()
+
+          const userInitialData = {
+            created_at: timestamp,
+            email: email,
+            role: 'customer',
+            uid: uid,
+            updated_at: timestamp,
+            username: username
+          }
+
+          // ユーザーのデータを登録する
+          db.collection('users').doc(uid).set(userInitialData)
+            .then(() => {
+              dispatch(push('/'))
+            })
+        }
+      })
   }
 }
