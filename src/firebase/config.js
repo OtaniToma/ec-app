76a21a843e2e2019a03614a24fe3b67b012baeaa
Toma Otani <otani.toma@gmail.com>
2020-06-23 11:43:37 -0700

    add firebase, clean up files

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDoqvnQw1BErcNb0KEgUhPrEgwVUR5XCxM",
+  authDomain: "ec-app-87855.firebaseapp.com",
+  databaseURL: "https://ec-app-87855.firebaseio.com",
+  projectId: "ec-app-87855",
+  storageBucket: "ec-app-87855.appspot.com",
+  messagingSenderId: "483640469181",
+  appId: "1:483640469181:web:46f43b9b971c57a1f9f9d4",
+  measurementId: "G-32SJ4X84SP"
+};
