168178cae4f83f69f9cb59dc3e4af75e396dcb19
Toma Otani <otani.toma@gmail.com>
2020-06-24 16:28:57 -0700

    add select box

--- src/templates/ProductEdit.jsx
+++ src/templates/ProductEdit.jsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useState } from 'react'
-import { TextInput } from '../components/UIkit'
+import { TextInput, SelectBox, PrimaryButton } from '../components/UIkit'
 
 const ProductEdit = () => {
 
@@ -21,6 +21,18 @@ const ProductEdit = () => {
     setPrice(event.target.value)
   }, [setPrice])
 
+  const categories = [
+    {id: 'tops', name: 'トップス'},
+    {id: 'shirts', name: 'シャツ'},
+    {id: 'pants', name: 'パンツ'},
+  ]
+
+  const genders = [
+    {id: 'all', name: 'すべて'},
+    {id: 'male', name: 'メンズ'},
+    {id: 'female', name: 'レディース'},
+  ]
+
   return (
     <section>
       <h2 className="u-text__headline  u-text-center">商品の登録・編集</h2>
@@ -32,6 +44,12 @@ const ProductEdit = () => {
         <TextInput
           fullWidth={true} label={'商品説明'} multiline={true} required={true}
           onChange={inputDescription} rows={5} value={description} type={'text'}
+        />
+        <SelectBox
+          label={'カテゴリー'} required={true} options={categories} select={setCategory} value={category}
+        />
+        <SelectBox
+          label={'性別'} required={true} options={genders} select={setGender} value={gender}
         />
         <TextInput
           fullWidth={true} label={'価格'} multiline={false} required={true}
