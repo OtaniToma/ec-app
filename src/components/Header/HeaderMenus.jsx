e001b262b1fd804a5e4c9de9eb7009b1932e6da4
Toma Otani <otani.toma@gmail.com>
2020-07-08 16:57:18 -0700

    add firebase listener

--- src/components/Header/HeaderMenus.jsx
+++ src/components/Header/HeaderMenus.jsx
@@ -1,15 +1,47 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import IconButton from '@material-ui/core/IconButton';
 import Badge from '@material-ui/core/Badge';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
 import MenuIcon from '@material-ui/icons/Menu';
-import { getProductsInCart } from '../../reducks/users/selectors';
+import { getUserId, getProductsInCart } from '../../reducks/users/selectors';
+import { db } from '../../firebase/index';
 
 const HeaderMenus = (props) => {
+  const dispatch = useDispatch();
   const selector = useSelector((state) => state);
+  const uid = getUserId(selector);
   let productsInCart = getProductsInCart(selector);
+
+  useEffect(() => {
+    const unsubscribe = db.collection('users').doc(uid).collection()
+    .onSnapshot(snapshots => {
+      snapshots.docChanges().forEach(change => {
+        const product = change.doc.data();
+        const changeType = change.type;
+
+        switch (changeType) {
+          case 'added':
+            productsInCart.push(product);
+            break;
+          case 'modified':
+            const index = productsInCart.findIndex(product => product.cartId === change.doc.id);
+            productsInCart[index] = product;
+            break;
+          case 'removed':
+            productsInCart = productsInCart.filter(product => product.cartId !== change.doc.id);
+            break;
+          default:
+            break;
+        }
+      })
+
+      dispatch(fetchProductsInCart(productsInCart));
+    })
+
+    return () => unsubscribe()
+  }, []);
 
   return (
     <>
